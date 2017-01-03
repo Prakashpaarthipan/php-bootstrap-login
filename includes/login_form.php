22b11edea9cd92ad6a39bfbefd71f23c385ed972
Rudy Martin <realrudymartin@gmail.com>
2017-01-03 10:01:21 -0800

    hard-code phplogin folder for images

--- includes/login_form.php
+++ includes/login_form.php
@@ -3,11 +3,11 @@
   <form method = "post" action="">
    <p>Simple Login Example</p>
    <div>
-   <input type = "image" id = "icon_user" class="icon_user" src="/assets/icon_user.png" alt="" height="25">
+   <input type = "image" id = "icon_user" class="icon_user" src="/phplogin/assets/icon_user.png" alt="" height="25">
    <input type = "text" id = "login" placeholder = "Username" name = "uid">
     </div>
     <div>
-        <input type = "image" id = "icon_pass" class="icon_pass" src="/assets/icon_pass.png" alt="" height="25">
+        <input type = "image" id = "icon_pass" class="icon_pass" src="/phplogin/assets/icon_pass.png" alt="" height="25">
    <input type = "password" id = "password" name = "upass" placeholder = "Password">
    </div>
    <input type = "hidden" id="hiddenField" name = "fuseaction" value = "manual">
